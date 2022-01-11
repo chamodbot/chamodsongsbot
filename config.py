399e2579311c307bcd3f7533c791255099b58b63
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-01-11 15:18:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,4 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 MUST_JOIN = os.environ.get('MUST_JOIN', None)
-    if MUST_JOIN.startswith("@"):
-        MUST_JOIN = MUST_JOIN[1:]
 OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
