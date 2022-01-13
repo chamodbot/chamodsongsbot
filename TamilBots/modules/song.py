727dedb1c213360659a68051af3fb1b35ce40594
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-01-13 18:38:38 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -1,5 +1,4 @@
 from pyrogram import Client, filters
-from bot import bot, max_file
 
 
 from pyrogram import filters, types
@@ -23,57 +22,70 @@ def yt_search(song):
         url = f"https://youtu.be/{video_id}"
         return url
 
+@app.on_message(filters.command(['yts', f'song']))
+def song(client, message):
 
-@app.on_message(filters.audio | filters.video | filters.voice)
-async def voice_handler(_, message):
-    file_size = message.audio or message.video or message.voice
-    if max_file < file_size.file_size :
-        await message.reply_text(
-            "**⚠️ Max file size has been reached.**"
+    user_id = message.from_user.id 
+    user_name = message.from_user.first_name 
+    rpk = "["+user_name+"](tg://user?id="+str(user_id)+")"
+
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply('**🚀 ᗪOᗯᑎᒪOᗩᗪIᑎᘜ ՏᗩᐯᗴᖇՏ ...**')
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        #print(results)
+        title = results[0]["title"][:40]       
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f'thumb{title}.jpg'
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, 'wb').write(thumb.content)
+
+
+        duration = results[0]["duration"]
+        url_suffix = results[0]["url_suffix"]
+        views = results[0]["views"]
+
+    except Exception as e:
+        m.edit(
+            "Sorry To Say but I can't find anything ❌!\n\nTry Another Keyword! Btw you spelled it properly 🤔?"
         )
+        print(str(e))
         return
-    file = await message.download(f'{bot.rnd_id()}.mp3')
-    r = (await bot.recognize(file)).get('track', None)
-    os.remove(file)
-    if r is None:
-        await message.reply_text(
-            '**⚠️ Cannot recognize the audio**'
-        )
-        return
-    out = f'**Title**: `{r["title"]}`\n'
-    out += f'**Artist**: `{r["subtitle"]}`\n'
-    buttons = [
-            [
-                types.InlineKeyboardButton(
-                    '🎼 Related Songs',
-                    switch_inline_query_current_chat=f'related {r["key"]}',
-                ),
-                types.InlineKeyboardButton(
-                    '🔗 Share',
-                    url=f'{r["share"]["html"]}'
-                )
-            ],
-            [
-                types.InlineKeyboardButton(
-                    '🎵 Listen',
-                    url=f'{r["url"]}'
-                )
-            ],        
-        ]
-    response = r.get('artists', None)
-    if response:
-        buttons.append(
-            [
-                types.InlineKeyboardButton(
-                    f'💿 More Tracks from {r["subtitle"]}',
-                    switch_inline_query_current_chat=f'tracks {r["artists"][0]["id"]}',
-                )
-            ]
-        )
-    await message.reply_photo(
-        r['images']['coverarthq'],
-        caption=out,
-        reply_markup=types.InlineKeyboardMarkup(buttons)
-    )
-    await status.delete()
-    os.remove(f"{str(user_id)}.mp3")
+    m.edit("**🍀 ᑌᑭᒪOᗩᗪIᑎᘜ ...**")
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f'[{title[:35]}]({link})\n◇ **Source**: `YouTube` \n\n◇ **song Duration**: `{duration}` \n\n◇ **Song Views**: `{views}` \n\n◇ **Uploaded By**: **@{BOT_USERNAME}** \n\n🔥 **[nikoneDevelopers   ✪](https://t.me/nikoneDevelopers) Corporation ©️** '
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
+        m.delete()
+    except Exception as e:
+        m.edit(e)
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
+
+
+# Funtion To Download Song
+async def download_song(url):
+    song_name = f"{randint(6969, 6999)}.mp3"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(song_name, mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+    return song_name
