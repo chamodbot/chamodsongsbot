594424df557a342c6b9beea33098d4b85114580f
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-01-28 22:37:25 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -31,7 +31,7 @@ async def song(client, message):
         await message.reply("**😶 Oops Not Found ...**")
         return ""
     status = await message.reply("**🚀 Downloading Savers ....**")
-    await status.edit(
+    await status.edit_message_reply_markup(
             InlineKeyboardMarkup([[InlineKeyboardButton("🍀 Uploading To Telegram ....", callback_data="down")]]))
     await status.edit_message_reply_markup("**🍀 Uploading To Telegram ...**")
     video_link = yt_search(args)
