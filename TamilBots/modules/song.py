2a6bb6ff952fc730ca558fbd9c4c4c1d7ad64509
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-01-28 23:31:21 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -56,7 +56,7 @@ async def song(client, message):
         duration=int(yt.length),
         title=str(yt.title),
         performer=str(yt.author),
-        caption=f"hi",
+        caption=f"\n\n◇───────────────◇\n\n**✅ Successfully Downloaded to MP3 🎵**\n\n🌺 Requestor : [Requestor](tg://settings)\n🌷 Downloaded by : [Music Finder Bot](https://t.me/The_song_finder_bot)\n[🍀 zoneunlimited 🍀](https://t.me/zoneunlimited)Corporation ©️\n\n◇───────────────◇\n\n",
         reply_to_message_id=message.message_id,
     )
     await status.delete()
