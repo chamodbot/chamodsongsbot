7aad030239e750515a4d7afc530db1639fc06706
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-01-29 09:24:47 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -26,7 +26,6 @@ async def song(client, message):
     chat_id = message.chat.id
     user_id = message.from_user["id"]
     add_chat_to_db(str(chat_id))
-    args = get_arg(message) + " " + "song"
     if args.startswith(" "):
         await message.reply("**😶 Oops Not Found ...**")
         return ""
