7b5747b571e9c1e7f5df54b088bed31860e6aa4c
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-01-30 23:01:00 +0530

    Create song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -84,7 +84,7 @@ async def video(client, message):
     chat_id = message.chat.id
     user_id = message.from_user["id"]
     add_chat_to_db(str(chat_id))
-    args = get_arg(message) + " " + "song"
+    args = get_arg(message) + " " + "video"
     if args.startswith(" "):
         await message.reply("**😶 Oops Not Found ...**")
         return ""
