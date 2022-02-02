fa57d601466a30996b8131bfa3076e1dc4855679
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-02-02 15:41:22 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -133,7 +133,7 @@ async def help(client, message):
     text="**🌷 Restarting Music Savers ....**"
     btn = InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="✪❖◈◈◈❖◈◈◈❖◈◈◈❖✪", callback_data="progress_msg")]])
-    await message.edit("**🌺 Restarting complicated ....**"
+    
     await message.reply(text, reply_markup=btn)
 
 @app.on_inline_query()
