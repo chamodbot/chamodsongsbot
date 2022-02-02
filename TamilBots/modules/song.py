b2ba297288f7ced47fb0f73c1fec4ec71c697353
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-02-02 12:42:06 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -125,6 +125,43 @@ async def video(client, message):
     os.remove(f"{str(user_id)}.mp4")
 
 
+@app.on_message(filters.create(ignore_blacklisted_users) & filters.command("tool"))
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 10
+
+    animation_ttl = range(0, 12)
+
+    # input_str = event.pattern_match.group(1)
+
+    await event.edit("Deploying...")
+
+    animation_chars = [
+        "**Heroku Connecting To Latest Github Build (chamoddeshanbot/tguserbot)**",
+        "**Build started by user** **{DEFAULTUSER}**",
+        "**Deploy** `535a74f0` **by user** **{MY BOSS}**",
+        "**Restarting Heroku Server...**",
+        "**State changed from up to starting**",
+        "**Stopping all processes with SIGTERM**",
+        "**Process exited with** `status 143`",
+        "**Starting process with command** `python3 -m stdborg`",
+        "**State changed from starting to up**",
+        "__INFO:Black Lightning:Logged in as 557667062__",
+        "__INFO:Black Lightning:Successfully loaded all plugins__",
+        "**Build Succeeded**",
+    ]
+
+    for i in animation_ttl:
+
+        await asyncio.sleep(animation_interval)
+
+        await event.edit(animation_chars[i % 12])
+
+
 @app.on_inline_query()
 async def inline(client: Client, query: InlineQuery):
     answers = []
