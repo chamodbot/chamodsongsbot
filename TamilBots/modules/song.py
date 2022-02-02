9b3e238ade83eac703bc0203db89b7b0bc0017b6
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-02-02 13:30:05 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -130,8 +130,8 @@ async def help(client, message):
     if message.from_user["id"] == OWNER_ID:
         await message.reply(owner_help)
         return ""
-    btn= text="**🌷 Restarting Music Savers ....**",
-    reply_markup=InlineKeyboardMarkup(
+    text="**🌷 Restarting Music Savers ....**",
+    btn = InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="✪❖◈◈◈❖◈◈◈❖◈◈◈❖✪", callback_data="progress_msg")]])
 
     await message.reply(text, reply_markup=btn)
