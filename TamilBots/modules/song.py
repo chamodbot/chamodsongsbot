30ab0c3b108633a88f80e6b12116a72b654de9df
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-02-02 23:30:48 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -129,8 +129,7 @@ async def video(client, message):
 
 @app.on_message(filters.create(ignore_blacklisted_users) & filters.command("tools"))
 async def song(client, message):
-    chat_id = message.chat.id
-    user_id = message.from_user["id"] not in AUTH_USERS:
+    if message.from_user.id not in AUTH_USERS:
         return ""
     add_chat_to_db(str(chat_id))
     args = get_arg(message) + " " + "song"
