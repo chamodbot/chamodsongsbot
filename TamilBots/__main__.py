240a0a8fec6d35e1f4b4539fe3859379402802b6
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-02-03 19:25:48 +0530

    Update __main__.py

--- TamilBots/__main__.py
+++ TamilBots/__main__.py
@@ -84,6 +84,7 @@ async def start(client, message):
     else:
         btn = None
     await message.reply(start_text, reply_markup=btn)
+    add_chat_to_db(str(chat_id))
 
 
 @app.on_message(filters.create(ignore_blacklisted_users) & filters.command("help"))
