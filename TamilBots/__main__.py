634deca9114900970215d9677f8fbbcef530e085
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-02-03 18:44:31 +0530

    Update __main__.py

--- TamilBots/__main__.py
+++ TamilBots/__main__.py
@@ -123,7 +123,7 @@ async def start(b, m):
                     parse_mode="HTML",
                     disable_web_page_preview=True)
                 return
-        await m.reply(start_text.format(user_id), reply_markup=btn)
+        await m.reply(start_text.format(m.from_user.mention), reply_markup=btn)
     add_chat_to_db(str(chat_id))
 
 
