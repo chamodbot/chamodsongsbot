5fb8d77b7f7f36a71bb9e48aba59d72a39e34913
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-02-05 11:40:39 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -85,11 +85,6 @@ async def song(client, message):
 @app.on_message(filters.create(ignore_blacklisted_users) & filters.command("song"))
 async def song(client, message):
     chat_id = message.chat.id
-    if message.from_user.id not in AUTH_USERS:
-        for chat in GROUP_ID:
-            try:
-               await message.reply("**⛔️ Access Denied ⛔️**\n\n**Please Contact** @chamod_deshan to **Get Access** or Join [zoneunlimited](https://t.me/zoneunlimited) to Access **This Service** 🌷")
-               return ""
     query = " ".join(message.text[1:])
     m = message.reply_chat_action("record_audio")
     m = message.reply("**🎵 Searching Music Savers ....**",
