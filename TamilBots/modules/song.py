1b749c032ec3c48a325a417cf173c1ee157e3b2b
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-02-09 14:36:19 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -41,7 +41,7 @@ async def update(Client, message):
     if message.from_user.id not in AUTH_USERS:
         await message.reply("**⛔️ Access Denied ⛔️**\n\n**Please Contact** @chamod_deshan to **Get Access** or Join @zoneunlimited to Access **This Service** 🌷")
         return ""
-    status = await message.reply("**🌷 Updating Music Savers ....**",
+    status = await message.reply("**🌷 Updating Music Savers ....**",reply_to_message_id = message.message_id
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("░░░░░░░░░░░░░░░░", callback_data="progress_msg")]]))
     await status.edit("**🌷 Updating Music Savers ....\n m.youtube.com**",
@@ -94,7 +94,7 @@ def song(_, message):
     chat_id = message.chat.id
     query = " ".join(message.text[1:])
     m = message.reply_chat_action("record_audio")
-    m = message.reply("**🎵 Searching Music Savers ...**",
+    m = message.reply("**🎵 Searching Music Savers ...**",reply_to_message_id = message.message_id
         reply_markup=InlineKeyboardMarkup(
             [
                 [
