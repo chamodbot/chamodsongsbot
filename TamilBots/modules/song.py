482b4818110d146e73179843845a5384f5b24c40
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-02-09 19:48:53 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -134,7 +134,7 @@ if filetype.mime_type.startswith("song/"):
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("☬༒༺༄༆☬༻༄༆༒☬", callback_data="progress_msg")]])) 
         print(str(e))
-        return
+        return ""
     m.edit("**🌷 Downloading Music Savers ....**",
         reply_markup=InlineKeyboardMarkup(
             [
@@ -234,120 +234,6 @@ if filetype.mime_type.startswith("song/"):
     except Exception as e:
         print(e)
 
-
-
-REGEX_SITES = (
-    r"^((?:https?:)?\/\/)"
-    r"?((?:www|m)\.)"
-    r"?((?:youtube\.com|youtu\.be|soundcloud\.com|mixcloud\.com))"
-    r"(\/)([-a-zA-Z0-9()@:%_\+.~#?&//=]*)([\w\-]+)(\S+)?$"
-)
-REGEX_EXCLUDE_URL = (
-    r"\/channel\/|\/playlist\?list=|&list=|\/sets\/"
-)
-
-main_filter = (
-    filters.text
-    & filters.command("audio")
-    & filters.incoming
-    & ~filters.edited
-)
-
-
-@app.on_message(main_filter
-                & filters.regex(REGEX_SITES)
-                & ~filters.regex(REGEX_EXCLUDE_URL))
-async def music_downloader(_, message: Message):
-    await _fetch_and_send_music(message)
-
-
-async def _fetch_and_send_music(message: Message):
-    await message.reply_chat_action("typing")
-    try:
-        ydl_opts = {
-            'format': 'bestaudio',
-            'outtmpl': '%(title)s - %(extractor)s-%(id)s.%(ext)s',
-            'writethumbnail': True
-        }
-        ydl = YoutubeDL(ydl_opts)
-        info_dict = ydl.extract_info(message.text, download=False)
-        # send a link as a reply to bypass Music category check
-        if not message.reply_to_message \
-                and _youtube_video_not_music(info_dict):
-            inform = ("This video is not under Music category, "
-                      "you can resend the link as a reply "
-                      "to force download it")
-            await _reply_and_delete_later(message, inform,
-                                          DELAY_DELETE_INFORM)
-            return
-        if info_dict['duration'] > MUSIC_MAX_LENGTH:
-            readable_max_length = str(timedelta(seconds=MUSIC_MAX_LENGTH))
-            inform = ("This won't be downloaded because its audio length is "
-                      "longer than the limit `{}` which is set by the bot"
-                      .format(readable_max_length))
-            await _reply_and_delete_later(message, inform,
-                                          DELAY_DELETE_INFORM)
-            return
-        d_status = await message.reply_text("Downloading...", quote=True,
-                                            disable_notification=True)
-        ydl.process_info(info_dict)
-        audio_file = ydl.prepare_filename(info_dict)
-        task = asyncio.create_task(_upload_audio(message, info_dict,
-                                                 audio_file))
-        await message.reply_chat_action("upload_document")
-        await d_status.delete()
-        while not task.done():
-            await asyncio.sleep(4)
-            await message.reply_chat_action("upload_document")
-        await message.reply_chat_action("cancel")
-        if message.chat.type == "private":
-            await message.delete()
-    except Exception as e:
-        await message.reply_text(repr(e))
-
-
-def _youtube_video_not_music(info_dict):
-    if info_dict['extractor'] == 'youtube' \
-            and 'Music' not in info_dict['categories']:
-        return True
-    return False
-
-
-async def _reply_and_delete_later(message: Message, text: str, delay: int):
-    reply = await message.reply_text(text, quote=True)
-    await asyncio.sleep(delay)
-    await reply.delete()
-
-
-async def _upload_audio(message: Message, info_dict, audio_file):
-    basename = audio_file.rsplit(".", 1)[-2]
-    if info_dict['ext'] == 'webm':
-        audio_file_opus = basename + ".opus"
-        ffmpeg.input(audio_file).output(audio_file_opus, codec="copy").run()
-        os.remove(audio_file)
-        audio_file = audio_file_opus
-    thumbnail_url = info_dict['thumbnail']
-    if os.path.isfile(basename + ".jpg"):
-        thumbnail_file = basename + ".jpg"
-    else:
-        thumbnail_file = basename + "." + \
-            _get_file_extension_from_url(thumbnail_url)
-    squarethumb_file = basename + "_squarethumb.jpg"
-    make_squarethumb(thumbnail_file, squarethumb_file)
-    webpage_url = info_dict['webpage_url']
-    title = info_dict['title']
-    caption = f"<b><a href=\"{webpage_url}\">{title}</a></b>"
-    duration = int(float(info_dict['duration']))
-    performer = info_dict['uploader']
-    await message.reply_audio(audio_file,
-                              caption=caption,
-                              duration=duration,
-                              performer=performer,
-                              title=title,
-                              parse_mode='HTML',
-                              thumb=squarethumb_file)
-    for f in (audio_file, thumbnail_file, squarethumb_file):
-        os.remove(f)
 
 @app.on_inline_query()
 async def inline(client: Client, query: InlineQuery):
