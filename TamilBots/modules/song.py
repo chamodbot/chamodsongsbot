dbd1d10d28a989cdd88d3a76c0956071ebb8f9e0
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-02-10 22:05:56 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -227,6 +227,66 @@ def song(_, message):
         print(e)
 
 
+@app.on_message(filters.command(["video"]))
+async def vsong(pbot, message):
+    ydl_opts = {
+        'format':'best',
+        'keepvideo':True,
+        'prefer_ffmpeg':False,
+        'geo_bypass':True,
+        'outtmpl':'%(title)s.%(ext)s',
+        'quite':True
+    }
+    query = " ".join(message.command[1:])
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"thumb{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        duration = results[0]["duration"]
+        views = results[0]["views"]
+        results[0]["url_suffix"]
+        results[0]["views"]
+        button = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("Watch On Youtube🎬", url=f"{link}")
+        ],
+        [
+            InlineKeyboardButton("Search here 🔎", switch_inline_query_current_chat="")
+        ]
+    ]
+    
+    )
+        rby = message.from_user.mention
+    except Exception as e:
+        print(e)
+    try:
+        msg = await message.reply("📥 **downloading video...**")
+        with YoutubeDL(ydl_opts) as ytdl:
+            rep = f'🏷 **Video name**: [{title[:35]}]({link})\n⏱️ **Video Duration**: `{duration}`\n👁‍🗨 **Video Views**: `{views}`\n**🎧 Requested by:** {message.from_user.mention}\n 🤟Downloaded By : @szsongbot '
+            ytdl_data = ytdl.extract_info(link, download=True)
+            file_name = ytdl.prepare_filename(ytdl_data)
+    except Exception as e:
+        return await msg.edit(f"❌**YouTube Download Error !*** {str(e)}\n\n Go support chat👉 @slbotzone")
+    preview = wget.download(thumbnail)
+    await msg.edit("📤 **uploading video...**")
+    await message.reply_video(
+        file_name,
+        duration=int(ytdl_data["duration"]),
+        thumb=preview,
+        caption=rep,
+        reply_markup= button)
+    try:
+        os.remove(file_name)
+        msg.delete()
+    except Exception as e:
+        print(e)
+        
+
 @app.on_inline_query()
 async def inline(client: Client, query: InlineQuery):
     answers = []
