a7bdfa8dc9681b0cfeadb59aa8124d370c7270fd
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-02-12 22:53:37 +0530

    Update song.py

--- TamilBots/modules/song.py
+++ TamilBots/modules/song.py
@@ -105,11 +105,9 @@ async def update(Client, message):
     await gift.delete()
 
 
-JOIN_ASAP = "**⛔️ Access Denied ⛔️**\n\n🙋‍♂️ **Hey There** ,{message.from_user.mention} You Must **Join** @Zu_Project Telegram **Channel** To Use This BOT. So, **Please Join** it & Try Again🤗. **Thank** You 🤝"
-
 FSUBB = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton(text="  Zu Project  🇱🇰  ", url=f"https://t.me/Zu_Project") 
+        InlineKeyboardButton(text="🍀  zoneunlimited  🍀", url=f"https://t.me/zoneunlimited") 
         ]]      
     )
 
@@ -118,10 +116,10 @@ FSUBB = InlineKeyboardMarkup(
 async def song(__, message):
     try:
         await message.reply_chat_action("import_history")
-        await message._client.get_chat_member(int("-1001563603672"), message.from_user.id)
+        await message._client.get_chat_member(int("-1001110021950"), message.from_user.id)
     except UserNotParticipant:
         await message.reply_text(
-        text=f"**⛔️ Access Denied ⛔️**\n\n🙋‍♂️ **Hey There** {message.from_user.mention}, You Must **Join** @Zu_Project Telegram **Channel** To Use This BOT. So, **Please Join** it & Try Again🤗. **Thank** You 🤝", disable_web_page_preview=True, reply_markup=FSUBB, reply_to_message_id = message.message_id
+        text=f"**⛔️ Access Denied ⛔️**\n\n🙋‍♂️ **Hey There** {message.from_user.mention}, You Must **Join** @zoneunlimited Telegram **Channel** To Use This BOT. So, **Please Join** it & Try Again🤗. **Thank** You 🤝", disable_web_page_preview=True, reply_markup=FSUBB, reply_to_message_id = message.message_id
     )
         return
     await message.reply_chat_action("typing")
@@ -189,191 +187,6 @@ async def song(__, message):
         print(e)
 
 
-@app.on_inline_query()
-async def inline(client: Client, query: InlineQuery):
-    answers = []
-    search_query = query.query.lower().strip().rstrip()
-
-    if search_query == "":
-        await client.answer_inline_query(
-            query.id,
-            results=answers,
-            switch_pm_text="🔍 Search YouTube 🔎",
-            switch_pm_parameter="help",
-            cache_time=0
-        )
-    else:
-        search = VideosSearch(search_query, limit=50)
-
-        for result in search.result()["result"]:
-            answers.append(
-                InlineQueryResultArticle(
-                    title=result["title"],
-                    description="{}, {} views.".format(
-                        result["duration"],
-                        result["viewCount"]["short"]
-                    ),
-                    input_message_content=InputTextMessageContent(
-                        "https://www.youtube.com/watch?v={}".format(
-                            result["id"]
-                        )
-                    ),
-                    thumb_url=result["thumbnails"][0]["url"]
-                )
-            )
-
-        try:
-            await query.answer(
-                results=answers,
-                cache_time=0
-            )
-        except errors.QueryIdInvalid:
-            await query.answer(
-                results=answers,
-                cache_time=0,
-                switch_pm_text="😶 Oops Not Found ...",
-                switch_pm_parameter="",
-            )
-
-JOIN_ASAP = "**⛔️ Access Denied ⛔️**\n\n🙋‍♂️ **Hey There** {message.from_user.mention}, You Must **Join** @Zu_Project Telegram **Channel** To Use This BOT. So, **Please Join** it & Try Again🤗. **Thank** You 🤝"
-
-FSUBB = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton(text="  Zu Project  🇱🇰  ", url=f"https://t.me/Zu_Project") 
-        ]]      
-    )
-
-# ====== SHAZAM ======
-
-async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
-    """ run command in terminal """
-    args = shlex.split(cmd)
-    process = await asyncio.create_subprocess_exec(
-        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    return (
-        stdout.decode("utf-8", "replace").strip(),
-        stderr.decode("utf-8", "replace").strip(),
-        process.returncode,
-        process.pid,
-    )
-
-async def fetch_audio(client, message):
-    time.time()
-    if not message.reply_to_message:
-        await message.reply("`Reply To A Video / Audio.`")
-        return
-    warner_stark = message.reply_to_message
-    if warner_stark.audio is None and warner_stark.video is None:
-        await message.reply("`Format Not Supported`")
-        return
-    if warner_stark.video:
-        lel = await message.reply("`Video Detected, Converting To Audio !`")
-        warner_bros = await message.reply_to_message.download()
-        stark_cmd = f"ffmpeg -i {warner_bros} -map 0:a friday.mp3"
-        await runcmd(stark_cmd)
-        final_warner = "friday.mp3"
-    elif warner_stark.audio:
-        lel = await message.reply_sticker(sticker = "CAACAgIAAxkBAAIDOmIDuTomMEzGzYgtoiiQj73c-8BrAAK6AAMw1J0RhNfEiMRQZ1YjBA", reply_to_message_id = message.message_id)
-        final_warner = await message.reply_to_message.download()
-    await lel.delete()
-    return final_warner
-
-
-async def edit_or_reply(message, text, parse_mode="md"):
-    if message.from_user.id:
-        if message.reply_to_message:
-            kk = message.reply_to_message.message_id
-            return await message.reply_text(
-                text, reply_to_message_id=kk, parse_mode=parse_mode
-            )
-        return await message.reply_text(text, parse_mode=parse_mode)
-    return await message.edit(text, parse_mode=parse_mode)
-
-@app.on_message(filters.command("find"))
-async def shazamm(client, message):
-    try:
-        await message.reply_chat_action("typing")
-        await message._client.get_chat_member(int("-1001563603672"), message.from_user.id)
-    except UserNotParticipant:
-        await message.reply_text(
-        text=f"**⛔️ Access Denied ⛔️**\n\n🙋‍♂️ **Hey There** {message.from_user.mention}, You Must **Join** @Zu_Project Telegram **Channel** To Use This BOT. So, **Please Join** it & Try Again🤗. **Thank** You 🤝", disable_web_page_preview=True, reply_markup=FSUBB, reply_to_message_id = message.message_id
-    )
-        return
-    await message.reply_chat_action("record_audio")
-    sz = await message.reply("**🎵 Sεαяcнıпɢ AυÐเO Ƒιℓє  ....**",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓", callback_data="progress_msg")]]), reply_to_message_id = message.message_id)
-    if not message.reply_to_message:
-        await sz.edit("**😶 Oops Not Found !! ....**",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓", callback_data="progress_msg")]]))
-        return
-    if os.path.exists("friday.mp3"):
-        os.remove("friday.mp3")
-    kkk = await fetch_audio(client, message)
-    downloaded_file_name = kkk
-    f = {"file": (downloaded_file_name, open(downloaded_file_name, "rb"))}
-    await message.reply_chat_action("record_audio")
-    await sz.edit("**🌷 ƊօωղƖօąɗíղɠ AυÐเO Ƒιℓє ....**",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓", callback_data="progress_msg")]]))
-    await sz.edit("**🍀 ᑌᑭᒪOᗩᗪIᑎG ᏆᎾ TᒪᕮGᖇᗩᗰ ....**",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓", callback_data="progress_msg")]]))
-    r = requests.post("https://starkapi.herokuapp.com/shazam/", files=f)
-    try:
-        xo = r.json()
-    except JSONDecodeError:
-        await sz.edit("`Seems Like Our Server Has Some Issues, Please Try Again Later!`")
-        return
-    if xo.get("success") is False:
-        await sz.edit("**😶 Oops Not Found !! ....**",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓", callback_data="progress_msg")]]))
-        os.remove(downloaded_file_name)
-        return
-    button = InlineKeyboardMarkup(
-    [
-        [
-            InlineKeyboardButton("  Zu Project  🇱🇰  ", url=f"https://t.me/Zu_Project")
-        ],
-        [
-            InlineKeyboardButton("🔍◇─◇Search Again◇─◇🔎", switch_inline_query_current_chat="")
-        ]
-    ]
-    
-    )
-    
-    xoo = xo.get("response")
-    zz = xoo[1]
-    zzz = zz.get("track")
-    zzz.get("sections")[3]
-    nt = zzz.get("images")
-    image = nt.get("coverarthq")
-    by = zzz.get("subtitle")
-    title = zzz.get("title")
-    messageo = f"""<b>✅ Successfully Download To Mp3 Ditels ..</b>
-┏━━━━━━━━━━━━━━━━━┓
-
-┣★ 🎵  Song Name :<b> {title} </b>
-
-┣★ 🌷 Channel Name :<b> {by} </b>
-
-┣★ 🚀 Requestor :<b> {message.from_user.mention} </b>
-
-┣★ 🍀 @Zu_Project 🇱🇰  <b> Corporation ©️ </b>
-
-┗━━━━━━━━━━━━━━━━━┛
-"""
-    await message.reply_chat_action("upload_photo")
-    await client.send_photo(message.chat.id, image, messageo, reply_markup=button, parse_mode="HTML")
-    os.remove(downloaded_file_name)
-    await sz.delete()
-
-
-
 @app.on_callback_query()
 async def button(app, update):
       cb_data = update.data
@@ -385,5 +198,3 @@ async def button(app, update):
       elif "zone_ms" in cb_data:
         await update.message.delete()
         await zone_ms(app, update.message)
-
-
